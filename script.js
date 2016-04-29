f88b2d24dd3c35094a6d071fcc5f9bfd551d90f6
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2016-04-29 11:19:30 -0700

    add gh-pages

---
+++ script.js
@@ -0,0 +1,108 @@
+
+(function () {
+
+  'use strict';
+  
+  init();
+  
+  function DomObj() {
+    var self = this;
+    var productsLoaded, totalCount;
+    self.products = [];
+
+    /* ----- PUBLIC ----- */
+    
+    self.loadProducts = function (url) {
+      productsLoaded = 0;
+      $.getJSON(url, function (response) {
+        totalCount = response.sales.length;
+        for (var i = 0; i < totalCount; i++) {
+          var product = new ProductObj(response.sales[i], i);
+          self.products.push(product);
+          product.updateHtml(asyncUpdateDom);
+        }
+      });
+    }
+
+    /* ----- PRIVATE ----- */
+    
+    function updateDom() {
+      var i = 0;
+      var thishtml = '';
+      for (var i = 0; i < self.products.length; i++) {
+        thishtml += self.products[i].htmlview;
+      }
+      $('#content').append(thishtml);
+      addEventListeners();
+      
+      //NOTE: setTimeout here to at least show half of loading rotation
+      setTimeout(removeLoadingOverlay, 1000);
+    }
+    
+    // This function is used as a callback for the ProductObj.updateHtml() function
+    // It will keep track of how many products have finished loading,
+    // and call updateDom() once everything is loaded
+    function asyncUpdateDom() {
+      productsLoaded++;
+      if (productsLoaded === totalCount) {
+        updateDom(); 
+      }
+    }
+    
+    function addEventListeners() {
+      //Remove product listener with "fade" effect
+      $('button.remove-btn').click(function() {
+        var wrapperEl = $(this).closest('.product-wrapper');
+        wrapperEl.addClass('fade-out');
+        setTimeout(function () {
+          wrapperEl.remove();
+        }, 250);  //setTimeout to let fade transition finish before removing element
+      });
+      
+      //Flip product card
+      $('button.flip-btn').click(function(evt) {
+        evt.stopPropagation();
+        evt.preventDefault();
+        
+        var wrapperEl = $(this).closest('.product-wrapper');
+        wrapperEl.toggleClass('flip-180');
+      });
+    }
+    
+    function removeLoadingOverlay() {
+      var overlayEl = $('.is-loading');
+      overlayEl.addClass('slide-up fade-out');
+      setTimeout(function () {
+        overlayEl.remove();
+      }, 500);  //setTimeout to let slide & fade transition finish before removing element
+    }
+  }
+
+  function ProductObj(product, i) {
+    var self = this;
+    self.photo = product.photos.medium_half;
+    self.title = product.name;
+    self.tagline = product.tagline;
+    self.url = product.url;
+    self.description = product.description;
+    self.htmlview = '';
+    self.index = i;
+    self.custom_class = '';
+
+    self.updateHtml = function (callback) {
+      $.get('product-template.html', function (template) {
+        self.htmlview = template.replace('{image}', self.photo).replace(/{title}/g, self.title).replace(/{tagline}/g, self.tagline).replace('{url}', self.url).replace('{custom_class}', self.custom_class).replace(/{description}/g, self.description);
+      
+        if (callback && typeof callback === 'function') {
+          callback();
+        }
+      });
+    }
+  }
+
+  function init() {
+    var page = new DomObj();
+    page.loadProducts('data.json');    
+  }
+  
+}());
